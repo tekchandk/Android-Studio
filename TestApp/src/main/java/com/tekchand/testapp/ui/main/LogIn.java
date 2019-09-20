68c365b985c79252cfb87eed062b47d0562b61fc
tekchandk <tekchandy14@gmail.com>
2019-09-20 14:14:36 +0530

    Created multiple session.

--- TestApp/src/main/java/com/tekchand/testapp/ui/main/LogIn.java
+++ TestApp/src/main/java/com/tekchand/testapp/ui/main/LogIn.java
@@ -1,8 +1,11 @@
 package com.tekchand.testapp.ui.main;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -45,10 +48,6 @@ public class LogIn extends AppCompatActivity {
         String email = emailText2.getEditText().getText().toString();
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
-
-
-
-
             loginBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -66,6 +65,12 @@ public class LogIn extends AppCompatActivity {
 
                 }
             });
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel =
+                    new NotificationChannel("MyNotifications", "MyNotifications", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
 
     }
 
